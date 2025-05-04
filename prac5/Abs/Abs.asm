bc96512a8d5f73a811b21b46eb82e61fa3c44b15
Nicholas Tran <nicholastran@Nicholass-MacBook-Air.local>
2025-05-04 22:15:52 +0930

    1

--- prac5/Abs/Abs.asm
+++ prac5/Abs/Abs.asm
@@ -2,3 +2,23 @@
 // (R0, R1 refer to RAM[0], and RAM[1], respectively.)
 
 // Put your code here.
+
+@1      // Load R1
+D=M     // D = R1
+
+@POSITIVE
+D;JGE   // If D >= 0, jump to POSITIVE
+
+// If R1 < 0, negate it
+D=-D    // D = -R1
+
+@0
+M=D     // Store abs(R1) in R0
+@END
+0;JMP
+
+(POSITIVE)
+@0
+M=D     // Store R1 in R0
+
+(END)
