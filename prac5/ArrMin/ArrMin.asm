778a946fd46142c2e4c6c9e05ba0b61a938765d0
Nicholas Tran <nicholastran@Nicholass-MacBook-Air.local>
2025-05-11 15:52:16 +0930

    1

--- prac5/ArrMin/ArrMin.asm
+++ prac5/ArrMin/ArrMin.asm
@@ -1,57 +1,55 @@
 // Finds the smallest element in the array of length R2 whose first element is at RAM[R1] and stores the result in R0.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-    @R1    // Load the address of the first element
-    D=M    // D = address of first element
-    A=D    // Set A register to that address
-    D=M    // D = first element value (initial min)
+    @R1
+    D=M    // D = base address
+    A=D    // A = base address
+    D=M    // D = first element
     @R0
-    M=D    // R0 = first element (assume it's the minimum)
+    M=D    // Store first element as current minimum
     
-    @R2    // Check if array has only one element
-    D=M    // D = length of array
+    @R2
+    D=M    // D = array length
     @END
-    D;JLE  // If length <= 1, we're done (edge case)
+    D;JLE  // If array length <= 0, we're done
     
-    @counter
-    M=D    // Initialize counter to array length
-    M=M-1  // Decrement because we already processed the first element
+    D=D-1  // Decrement length (we already processed first element)
+    @i
+    M=D    // i = array length - 1
     
     @R1
     D=M    // D = base address
-    @currentAddress
-    M=D    // Store base address in currentAddress
+    @addr
+    M=D    // Store base address
     
 LOOP:
-    @counter
+    @i
     D=M    // D = counter
     @END
     D;JLE  // If counter <= 0, we're done
     
-    @currentAddress
-    M=M+1  // Move to next element in array
-    A=M    // Set A to point to that element
-    D=M    // D = current element value
+    @addr
+    M=M+1  // Move to next element
+    A=M    // A = address of current element
+    D=M    // D = current element
     
-    @R0
-    D=D-M  // D = current element - current minimum
+    @R0    // Compare with current minimum
+    D=D-M  // D = current element - min
     @SKIP
-    D;JGE  // If current element >= minimum, skip the update
+    D;JGE  // If current element >= min, skip
     
-    // Update minimum
-    @currentAddress
-    A=M    // Get address of current element again
-    D=M    // D = current element value
-    @R0
+    @addr
+    A=M    // A = address of current element
+    D=M    // D = current element
+    @R0    
     M=D    // Update minimum
     
 SKIP:
-    @counter
+    @i
     M=M-1  // Decrement counter
     @LOOP
-    0;JMP  // Go back to loop
+    0;JMP  // Continue loop
     
 END:
-    // R0 already contains the minimum value
     @END
     0;JMP  // Infinite loop to end program
